48de40c672c40373841b56982d74e18d86a398e1
Jorge Mora <telejam@hotmail.com>
2021-10-12 15:42:16 -0300

    Se suben los archivos de entrada al path archivos/in/ y la captura del diagrama con las correcciones hechas

--- src/tierramedia/Promotor.java
+++ src/tierramedia/Promotor.java
@@ -2,6 +2,28 @@ package tierramedia;
 
 public class Promotor {
 	public static void main(String[] args) {
-		System.out.println("¡¡¡Bienvenidos a la Tierra Media!!!");
+		private ListadoUsuarios usuarios = new ListadoUsuarios();
+		private PaqueteAtracciones atracciones = new PaqueteAtracciones();
+		private PaquetePromociones promociones = new PaquetePromociones();
+		private Itinerario = new Itinerario();
+		
+		for (Usuario usuario : usuarios) {
+			
+//	Sugerir Atracciones
+			List<Promocion> listaPromociones = promociones.buscarPromociones();
+//	Procesarlas
+			List<Atraccion> listaAtraccionesMasCaras = atracciones.buscarAtraccionesMasCaras();
+//	Procesarlas
+			List<Atraccion> listaAtracionesMasLargas = atracciones.buscarAtracionesMasLargas();
+//	Procesarlas
+
+//			boolean? verificarUsuarioTiempo(){}
+//			boolean? verificarUsuarioPresupuesto(){}
+//			boolean? verificarAtraccionCupo(){}
+//			boolean verificarYaCargada(){}
+//			si (aceptarSugerencia()) void agregarAlItinerario(){}
+
+		}
+		
 	}
 }
